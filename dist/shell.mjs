7ed422e052331714b0c7b135b253339eafef90df
ashen <ayuanshen.liu@gmail.com>
2025-02-20 13:53:02 +0800

    cc

---
+++ dist/shell.mjs
@@ -0,0 +1,20 @@
+
+// ==UserScript==
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  site.zxhy
+// @author       ashen
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js
+// @require      file://D:\workspace\crawler\rpc-server\tampermonkey_shell\xx.js
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mouser.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+        
+(function () {
+	'use strict';
+
+
+
+})();
